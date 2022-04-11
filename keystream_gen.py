27284ca5f4e6beed4720d9bcb89a57872fe4f3f9
sina-negarandeh <negarandeh.sina@gmail.com>
2022-04-11 22:24:02 +0430

    Add A5_1 class

--- keystream_gen.py
+++ keystream_gen.py
@@ -47,3 +47,59 @@ class LFSR:
             self.register.pop(-1)
             self.register.insert(0, feedback)
         return output
+
+
+class A5_1:
+    _19_bits_register_size = 19
+    _22_bits_register_size = 22
+    _23_bits_register_size = 23
+
+    _19_bits_register_clocking_bit = 8
+    _22_bits_register_clocking_bit = 10
+    _23_bits_register_clocking_bit = 10
+
+    _19_bits_register_tapped_bits = [18, 17, 16, 13]
+    _22_bits_register_tapped_bits = [21, 20]
+    _23_bits_register_tapped_bits = [22, 21, 20, 7]
+
+    def __init__(self):
+        self._19_bits_register = LFSR(self._19_bits_register_size, self._19_bits_register_clocking_bit, self._19_bits_register_tapped_bits)
+        self._22_bits_register = LFSR(self._22_bits_register_size, self._22_bits_register_clocking_bit, self._22_bits_register_tapped_bits)
+        self._23_bits_register = LFSR(self._23_bits_register_size, self._23_bits_register_clocking_bit, self._23_bits_register_tapped_bits)
+    
+    def initialize_registers(self, session_key):
+        self._19_bits_register.initialize(session_key[0:19])
+        self._22_bits_register.initialize(session_key[19:41])
+        self._23_bits_register.initialize(session_key[41:64])
+    
+    def cal_majority_bit(self):
+        sum = 0
+        sum += self._19_bits_register.get_clocking_bit()
+        sum += self._22_bits_register.get_clocking_bit()
+        sum += self._23_bits_register.get_clocking_bit()
+
+        if sum > 1:
+            return 1
+        else:
+            return 0
+    
+    def clock(self):
+        output = 0
+        majority_bit = self.cal_majority_bit()
+
+        output = output ^ self._19_bits_register.shift(majority_bit)
+        output = output ^ self._22_bits_register.shift(majority_bit)
+        output = output ^ self._23_bits_register.shift(majority_bit)
+
+        return output
+
+    def initialize(self, session_key):
+        Validator.validate_session_key(session_key)
+        self.initialize_registers(session_key)
+
+    def generate_key_stream(self, text_length):
+        key_stream = ""        
+
+        for _ in range(text_length):
+            key_stream += str(self.clock())
+        return key_stream
